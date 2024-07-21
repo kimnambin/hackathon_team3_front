730e197581b3c6fb8ec4305abe7f1bb11b642401
beom-jun <chlrhwns1@naver.com>
2024-07-21 16:37:07 +0900

    관리자 페이지 2차 수정, 3차 수정 필요

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,31 @@
 import React from 'react'
 import styles from "./Fixed.module.css"
+import { useNavigate } from 'react-router-dom'
 
 const Footer = () => {
+  const Navigate = useNavigate()
+
+  const goToManager=()=>{
+    Navigate('/manager')
+  }
   return (
     <footer>
 
-      <div className={styles.inner}>
+      <div className={styles.inner1}>
+        <p>끄적임</p>
+      </div>
+
+      <div className={styles.inner2}>
         <p>Project Manager 오한솔  | Frontend 김범준, 김남빈  |  Backend 노주희, 오유진 </p>
         
         <ul>
           <li>@성결대 멋쟁이사자처럼 12기 | 해커톤 3팀</li>
         </ul>
+      </div>
+
+      <div className={styles.inner3}>
+        <p>문의하기</p>
+        <p onClick={goToManager}>관리자 페이지</p>
       </div>
 
     </footer>
